93f0e28a4f2cb8c6097b4e5bb5d95134b6675b5b
ThienTT <3ts.coups@gmail.com>
2026-03-02 22:18:47 +0700

    Init Spring Boot project structure

---
+++ src/test/java/com/gym/GymManagementSystem/GymManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gym.GymManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
