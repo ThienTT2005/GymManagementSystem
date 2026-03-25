eb5cabb9e295895742656eab127993286894db71
uyengoc925 <uyenngoc925@gmail.com>
2026-03-25 19:14:16 +0700

    dot

--- src/main/java/com/gym/GymManagementSystem/repository/ClubRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gym.GymManagementSystem.repository;
-
-import com.gym.GymManagementSystem.model.Club;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClubRepository extends JpaRepository<Club, Integer> {
-}
--- src/main/webapp/components/sidebar.jsp
+++
@@ -1,6 +0,0 @@
-<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
-    <link rel="stylesheet" href="${pageContext.request.contextPath}/css/style.css">
-    <div id="sidebar" class="sidebar">
-
-
-    </div>
